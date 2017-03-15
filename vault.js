bee8a81c442636df4d8a88794c5f0a3438a54904
ctsygiel <ctsygiel@gmail.com>
2017-03-14 19:49:16 -1000

    All tests pass

--- vault.js
+++ vault.js
@@ -1,4 +1,24 @@
 'use strict';
 module.exports = function() {
-  
+
+  //PRIVATE
+  var myVault = {};
+
+  //PUBLIC
+  function setValue(key, value){
+    myVault[key] = value;
+  }
+
+ function getValue(key){
+    if(myVault[key]){
+      return myVault[key];
+    }
+    else{
+      return null;
+    }
+  }
+  return{
+    getValue,
+    setValue
+  };
 };
